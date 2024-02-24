7b7e1ce17715feda0efceb2e9b23a20da48c51bf
Eason0319 <103716007+Eason0319@users.noreply.github.com>
2024-02-24 13:42:38 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.TextStyle}>數位三甲 111019039 蘇翊宸</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  TextStyle: {
+    color: 'orange',
+    fontSize: 80,
+  },
+});
